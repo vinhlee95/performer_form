caa6ca29d3baf94683f2e988584b63943545ab66
Vinhlee95 <lethanhvinh95@gmail.com>
2018-05-10 16:09:26 +0300

    Added logos on the form

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
 import Input from './UI/Input/Input';
 import classes from './App.css';
+import InfoLogo from '../images/information-button.svg';
+import LocationLogo from '../images/location-button.svg';
+import CheckLogo from '../images/check-button.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <form className={classes.Form} >
-          <h2>Käyttäjätiedot:</h2>
+          <div className={classes.title}>
+            <img src={InfoLogo} alt="infoLogo" />
+            <h2>Käyttäjätiedot:</h2>
+          </div>
           <div className={classes.nameRow}>
             <div className={classes.Field} >
-              <label>Etunimi</label>
-              <Input placeholder="Sukunimi" />
+              <label>Etunimi:</label>
+              <Input placeholder="Etunimi" />
             </div>
 
             <div className={classes.Field}>
-              <label>Sukunimi</label>
+              <label>Sukunimi:</label>
               <Input placeholder="Sukunimi" />
             </div>
           </div>
-
-          <h2>Kotipaikka</h2>
+          
+          <div className={classes.title}>
+            <img src={LocationLogo} alt="locationLogo" />
+            <h2>Kotipaikka</h2>
+          </div>
           <div className={classes.addressRow} >
             <div className={classes.Field} >
-              <label>Katuosoite</label>
+              <label>Katuosoite:</label>
               <Input />
             </div>
             <div className={classes.Field} >
@@ -42,6 +51,11 @@ class App extends Component {
               <Input />
             </div>
           </div>
+
+          <button type="submit" className={classes.submitButton} >
+          <img src={CheckLogo} alt="checkLogo" />
+          Tallenna tiedot
+          </button>
         </form>
       </div>
     );
